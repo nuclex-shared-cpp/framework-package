d05e8292eaf8cf1051de98528ec3870bf7fbaea5
cygon <cygon@nuclex.org>
2024-09-04 13:59:03 +0200

    Added scripts to clean all build outputs

---
+++ clean-all-build-outputs.cmd
@@ -0,0 +1,20 @@
+::!%WinDir%\System32\cmd.exe
+@ECHO OFF
+setlocal enabledelayedexpansion
+
+set dirs=
+
+for /d %%D in (".\*") do (
+	set dirs=!dirs! "%%D\bin"
+)
+for /d %%D in (".\third-party\*") do (
+	set dirs=!dirs! "%%D\bin"
+)
+for %%D in (%dirs%) do (
+  if exist "%%D" (
+    rmdir /s /q "%%D"
+  )
+)
+
+endlocal
+
